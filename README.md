4d4b4e08a733ee2fe94bf6917c330e4f3643cf52
Shane Devane (SD-WIN) <shanedevane@gmail.com>
2016-11-16 10:16:32 +0000

    initial megapixel calc

---
+++ README.md
@@ -0,0 +1,33 @@
+# python-photo-resolution-comparison
+Package that compares a photos dimensions against known image sizes and 
+ratios to gain insight into potential source. 
+Newslinn http://www.newslinn.com
+
+Example: if a image conforms to the resolution of a common 'film' format
+ie. 16:9 that image might be a screenshot from a video (instead of a photo
+from a camera)
+
+# PURPOSE & GOALS
+
+## Purpose
+To be a package within a bigger system. This is designed around extracting as much
+image data from a photograph. As it's purpose is to aid in investigating 
+correlations between image resolutions as part of image classification.
+
+## Goal
+To determine if the photo resolution of an image aids in classification.
+
+# Reference
+
+### Megapixel table
+https://photographyicon.com/megapixel-table/
+
+## OTHER USEFUL ITEMS
+
+### Cookiecutter for initial project files and configuration
+https://github.com/audreyr/cookiecutter-pypackage
+
+
+### Aspect ratios, mostly of video camera
+https://en.wikipedia.org/wiki/Aspect_ratio_(image)#4:3_standard
+
--- python_photo_resolution_comparison/python_photo_resolution_comparison.py
+++ python_photo_resolution_comparison/python_photo_resolution_comparison.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import os
-import exifread
-from PIL import Image, ImageStat, ImageChops, ImageEnhance
-from PIL.ExifTags import TAGS, GPSTAGS
+from PIL import Image
 import json
-from fractions import Fraction
-from os.path import join, getsize
+import math
+from os.path import join
 
 # A lot of times the photo will be cropped such that it no longer conforms to a 4:3 or 16:9 standard. That's one way.
+# DVD producers can also choose to show even wider ratios such as 1.85:1 and 2.39:1[1] within the 16:9 DVD frame by hard matting or adding black bars within the image itself.
 
 
 class PythonPhotoResolutionComparison():
@@ -18,6 +17,7 @@ class PythonPhotoResolutionComparison():
         self._json_data = dict()
         self._image = None
         self._pil_tags = dict()
+        self._json_data['file_path'] = file_path
 
     @property
     def json(self):
@@ -32,29 +32,51 @@ class PythonPhotoResolutionComparison():
         gcd = self._recursive_gcd(width, height)
         return '{0}:{1}'.format(int(width/gcd), int(height/gcd))
 
+    def _get_megapixel(self, width, height):
+        pixels = width * height
+        exact = pixels
+        one_million = 1000000
+
+        if pixels > one_million:
+            round_up = math.ceil(pixels / one_million) * one_million
+            formatted = round_up / 1000000
+            normalised = formatted
+        else:
+            formatted = math.ceil(one_million / pixels)
+            normalised = 1.0
+
+        return exact, formatted, normalised
+
     def execute(self):
         self._image = Image.open(self._file_path)
         width, height = self._image.size
-        image_aspect_ratio = self._get_aspect_ratio(width, height)
+        aspect_ratio = self._get_aspect_ratio(width, height)
+        exactpixels, megapixel, normalised = self._get_megapixel(width, height)
 
-        # self._json_data['ratio'] = ratio
-        self._json_data['image_aspect_ratio'] = image_aspect_ratio
+        self._json_data['normalised_mega_pixels'] = normalised
+        self._json_data['exact_pixels'] = exactpixels
+        self._json_data['megapixel'] = megapixel
+        self._json_data['aspect_ratio'] = aspect_ratio
         self._json_data['width'] = width
         self._json_data['height'] = height
 
 
-        # self._get_exif_data()
-        # self._get_gps_data()
-        # self._store_gps_data()
+        # self._json_data['is_hdv_aspect_ratio'] = height
+        # self._json_data['is_square_ratio'] = height
+        # self._json_data['is_tv_or_classic_pc_ratio'] = height
+        # self._json_data['is_hd_video_or_us_tv'] = height
+
+
 
 if __name__ == "__main__":
 
-    extractor = PythonPhotoResolutionComparison('../Resources/IMG_0829.JPG')
-    extractor.execute()
-    print(extractor.json)
-    exit()
+    # extractor = PythonPhotoResolutionComparison('../Resources/IMG_0829.JPG')
+    # extractor.execute()
+    # print(extractor.json)
+    # exit()
 
-    directory = r'C:\code\python-photo-resolution-comparison\Resources\test_images'
+    # directory = r'C:\code\python-photo-resolution-comparison\Resources\test_images'
+    directory = r'C:\code\python-photo-resolution-comparison\Resources\more_tests'
     for filename in os.listdir(directory):
         if filename.endswith('jpg'):
             file_path = join(directory, filename)
